baa527be64e837c2a9ac750d7242eba466c0628b
Rajarshi Goswami <rajarshigoswami@Rajarshis-MacBook-Air.local>
2020-08-04 23:02:13 +0530

    Initial Commit

---
+++ src/public/client.js
@@ -0,0 +1,117 @@
+let store = {
+    user: { name: "Student" },
+    apod: [],
+    defaultTab: "Curiosity",
+    rovers: ["Curiosity", "Opportunity", "Spirit"],
+};
+
+// add our markup to the page
+const root = document.getElementById("root");
+
+const updateStore = (store, newState) => {
+    store = Object.assign(store, newState);
+    render(root, store);
+};
+
+const render = async (root, state) => {
+    root.innerHTML = App(state);
+    showSlides(1);
+};
+
+// create content
+const App = (state) => {
+    let { defaultTab } = store;
+    const images = state[defaultTab] || [];
+
+    return `
+        <header></header>
+        <main>
+            <section>
+                <div class="tab">
+                    <button class="tablinks ${
+                        defaultTab == "Curiosity" ? "active" : ""
+                    }" onclick="switchRover(event, 'Curiosity')">Curiosity</button>
+                    <button class="tablinks ${
+                        defaultTab == "Opportunity" ? "active" : ""
+                    }" onclick="switchRover(event, 'Opportunity')">Opportunity</button>
+                    <button class="tablinks ${
+                        defaultTab == "Spirit" ? "active" : ""
+                    }" onclick="switchRover(event, 'Spirit')">Spirit</button>
+                </div>
+                <div id="${defaultTab}" class="tabcontent active">
+                    ${images.length > 0 ? ImageCarousel(images.slice(0, 10)) : LoadingContainer()}
+                </div>
+            </section>
+        </main>
+        <footer></footer>
+    `;
+};
+
+// listening for load event because page should load before any JS is called
+window.addEventListener("load", () => {
+    render(root, store);
+});
+
+const switchRover = (Event, tab) => {
+    updateStore(store, {
+        defaultTab: tab,
+    });
+};
+
+const plusSlides = (n) => {
+    showSlides((slideIndex += n));
+};
+
+const currentSlide = (n) => {
+    showSlides((slideIndex = n));
+};
+let slideIndex = 1;
+const showSlides = (n) => {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (slides.length === 0) return;
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+};
+
+const ImageCarousel = (images) => {
+    const imageLength = images.length;
+    const carousel = images.map((image, idx) => {
+        return `<div class="mySlides fade">
+                <div class="numbertext">${idx + 1} / ${imageLength}</div>
+                <img src="${image.img_src}" style="width:100%">
+                <div class="text">
+                ${image.rover.name} - ${image.camera.full_name}
+                <p> Launch Date: ${image.rover.launch_date}</p>
+                <p> Landing Date: ${image.rover.landing_date} </p>
+                <p> Status: ${image.rover.status}</p>
+                <p> Date: ${image.earth_date}</p>
+                </div>
+            </div>`;
+    });
+
+    return `<div class="slideshow-container"> ${carousel.join("")} <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a> </div>`;
+};
+const LoadingContainer = () => {
+    const { defaultTab } = store;
+    if (!store[defaultTab]) {
+        getImageOfTheDay(store);
+    }
+    return `<div class="loader"></div>`;
+};
+const getImageOfTheDay = (state) => {
+    let { defaultTab } = state;
+    const currDate = new Date().toISOString().split("T")[0];
+    fetch(`http://localhost:3000/image?curr_date=${currDate}&rover=${defaultTab.toLowerCase()}`)
+        .then((res) => res.json())
+        .then((apod) => updateStore(store, { [defaultTab]: apod.data.photos }));
+};
---
+++ src/server/index.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/", express.static(path.join(__dirname, "../public")));
+
+// your API calls
+
+// example API call
+app.get("/apod", async (req, res) => {
+    try {
+        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`).then((res) =>
+            res.json()
+        );
+        res.send({ image });
+    } catch (err) {
+        console.log("error:", err);
+    }
+});
+
+app.get("/image", async (req, res) => {
+    console.dir(req.query);
+    const curr_date = req.query.curr_date;
+    const rover = req.query.rover;
+    try {
+        let data = await fetch(
+            `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${curr_date}&api_key=${process.env.API_KEY}&sol=2000`
+        ).then((res) => res.json());
+        res.send({ data });
+    } catch (err) {
+        console.log("error:", err);
+    }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
